226bd60bcb68ad164da1b0ff4b6538a1bd9ee6c7
Jason Noble <github+jasonn@jasonnoble.org>
2015-03-23 20:38:01 -0600

    Slide 49

---
+++ spec/features/shopping_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+feature "Shopping site" do
+  scenario "User views a list of products" do
+    product = FactoryGirl.create(:product)
+    product2 = FactoryGirl.create(:product)
+
+    visit "/"
+
+    expect(page).to have_content(product.name)
+    expect(page).to have_content(product.description)
+    expect(page).to have_image(product.image_url)
+    expect(page).to have_content(product.price)
+
+    expect(page).to have_content(product2.name)
+    expect(page).to have_content(product2.description)
+    expect(page).to have_image(product2.image_url)
+    expect(page).to have_content(product2.price)
+  end
+end
